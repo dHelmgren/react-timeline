9842912b4875af44512858e584f03f4bca81ecd0
dHelmgren <devincollege@gmail.com>
2020-04-15 14:09:15 -0700

    Update Timeline.js

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  
-  return;
-}
+const Timeline = (props) => {
+  const events = props.events.map((event) => {
+    return (<TimelineEvent
+      key={event.timeStamp}
+      person={event.person}
+      status={event.status}
+      time={event.timeStamp}
+      updateTimeline={event.updateTimeline} />);
+  });
+
+  return (
+    <section className='timeline'>
+      {events}
+    </section>
+  );
+};
 
 export default Timeline;
